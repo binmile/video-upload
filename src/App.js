9b897ddd9f2e41fae238be7f9d787bbb9be7b379
prakashgupta <prakash@binmile.com>
2023-10-05 17:28:07 +0530

    removedPkg

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import VideoUpload from "./screens/VideoUpload/VideoUpload";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import VideoUploadSecond from "./screens/VideoUpload/VideoUploadSecond";
+// import VideoUploadSecond from "./screens/VideoUpload/VideoUploadSecond";
 
 const App = () => {
   return (
